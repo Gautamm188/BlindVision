f856a8956c15c3a2fb3cece9246ab7cc6f821d1f
Gautamm188 <114243701+Gautamm188@users.noreply.github.com>
2023-04-08 16:22:12 +0530

    Add files via upload

---
+++ liveCamera.py
@@ -0,0 +1,6 @@
+from ultralytics import YOLO
+from ultralytics.yolo.v8.detect.predict import DetectionPredictor
+import cv2
+
+model = YOLO("yolov8s.pt")
+model.predict(source="0", show=True, conf=0.5)
---
+++ textToSpeech.py
@@ -0,0 +1,4 @@
+import pyttsx3
+engine = pyttsx3.init()
+engine.say("Bahut maza aara hai")
+engine.runAndWait()
